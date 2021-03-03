4874d39eb3a6fea7003c383ef36a911061bbdf69
SofyaShestakova <soneshya@mail.ru>
2021-03-03 11:14:34 +0300

    add parametrized test

--- src/test/SecTests.java
+++ src/test/SecTests.java
@@ -2,63 +2,80 @@ package test;
 
 import main.Function;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 
 import static java.lang.Math.PI;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SecTests{
+public class SecTests {
     double delta = 0.000001;
+
     @Test
-    void testLeftPartOfPositiveValues(){
+    void testLeftPartOfPositiveValues() {
         double result_of_sec_function;
         double input = 0;
-        double expectedOutput = 1/Math.cos(input);
+        double expectedOutput = 1 / Math.cos(input);
         result_of_sec_function = Function.sec(input);
-        assertEquals(result_of_sec_function, expectedOutput,delta);
+        assertEquals(result_of_sec_function, expectedOutput, delta);
     }
+
     @Test
-    void testRightPartOfPositiveValues(){
-        double input = PI/3;
+    void testRightPartOfPositiveValues() {
+        double input = PI / 3;
         double result_of_sec_function = Function.sec(input);
-        double expectedOutput = 1/Math.cos(input);
-        assertEquals(result_of_sec_function, expectedOutput,delta);
+        double expectedOutput = 1 / Math.cos(input);
+        assertEquals(result_of_sec_function, expectedOutput, delta);
     }
+
     @Test
-    void testLeftPartOfNegativeValues(){
-        double input = 2*PI/3;
+    void testLeftPartOfNegativeValues() {
+        double input = 2 * PI / 3;
         double result_of_sec_function = Function.sec(input);
-        double expectedOutput = 1/Math.cos(input);
-        assertEquals(result_of_sec_function, expectedOutput,delta);
+        double expectedOutput = 1 / Math.cos(input);
+        assertEquals(result_of_sec_function, expectedOutput, delta);
     }
+
     @Test
-    void testRightPartOfNegativeValues(){
-        double input = 3*PI/4;
+    void testRightPartOfNegativeValues() {
+        double input = 3 * PI / 4;
         double result_of_sec_function = Function.sec(input);
-        double expectedOutput = 1/Math.cos(input);
-        assertEquals(result_of_sec_function, expectedOutput,delta);
+        double expectedOutput = 1 / Math.cos(input);
+        assertEquals(result_of_sec_function, expectedOutput, delta);
     }
+
     @Test
-    void testAnotherValue(){
-        double input = 7*PI/6;
+    void testAnotherValue() {
+        double input = 7 * PI / 6;
         double result_of_sec_function = Function.sec(input);
-        double expectedOutput = 1/Math.cos(input);
-        assertEquals(result_of_sec_function, expectedOutput,delta);
+        double expectedOutput = 1 / Math.cos(input);
+        assertEquals(result_of_sec_function, expectedOutput, delta);
     }
+
     @Test
-    void testIncorrectValue(){
-        double input = PI/2;
+    void testIncorrectValue() {
+        double input = PI / 2;
         assertThrows(ArithmeticException.class, () -> Function.sec(input));
     }
+
     @Test
-    void testIncorrectValue_2(){
-        double input = 3*PI/2;
+    void testIncorrectValue_2() {
+        double input = 3 * PI / 2;
         assertThrows(ArithmeticException.class, () -> Function.sec(input));
     }
+
     @Test
-    void testStringValue(){
+    void testStringValue() {
         String input = "heeeyyouuu9";
         assertThrows(IllegalArgumentException.class, () -> Function.sec(Double.parseDouble(input)));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "test.csv", delimiter = ';')
+    public void csv_test(double expected, double x) {
+        double actual = Function.sec(x);
+        assertEquals(expected, actual);
     }
 
 }
